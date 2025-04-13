714d2d0d05a6b4afb99785bc199452c4b2296fd6
Damien Rembert <damien.rembert@gmail.com>
2025-04-13 18:57:21 +0100

    add typing

--- easy-snapshot.ts
+++ easy-snapshot.ts
@@ -1,7 +1,7 @@
-const path = require("path");
-const fs = require("fs");
+import path from "path";
+import fs from "fs";
 
-function checkSnapshot(testableFileObject) {
+function checkSnapshot(testableFileObject: TestableFileObject) {
   describe(testableFileObject.relative + " Snapshot", () => {
     it("should match the saved snapshot", () => {
       const htmlContent = fs.readFileSync(testableFileObject.absolute, "utf8");
@@ -11,7 +11,7 @@ function checkSnapshot(testableFileObject) {
   });
 }
 
-function isTestableFile(filePath) {
+function isTestableFile(filePath: string) {
   let isFile = fs.existsSync(filePath) && fs.lstatSync(filePath).isFile();
   if (isFile) {
     return path.extname(filePath).toLowerCase() === ".html";
@@ -19,41 +19,30 @@ function isTestableFile(filePath) {
   return false;
 }
 
-function findTestableFiles(directory) {
-  try {
-    const entries = fs.readdirSync(directory);
+function findTestableFiles(directory: string): string[] {
+  const entries = fs.readdirSync(directory);
 
-    let testableFiles = [];
+  let testableFiles = [];
 
-    for (const entry of entries) {
-      const entryPath = path.join(directory, entry);
+  for (const entry of entries) {
+    const entryPath = path.join(directory, entry);
 
-      if (fs.existsSync(entryPath) && fs.lstatSync(entryPath).isDirectory()) {
-        testableFiles.push(...findTestableFiles(entryPath));
-      } else if (isTestableFile(entryPath)) {
-        testableFiles.push(entryPath);
-      }
+    if (fs.existsSync(entryPath) && fs.lstatSync(entryPath).isDirectory()) {
+      testableFiles.push(...findTestableFiles(entryPath));
+    } else if (isTestableFile(entryPath)) {
+      testableFiles.push(entryPath);
     }
-    return testableFiles;
-  } catch (error) {
-    console.error(`Error reading directory: ${error.message}`);
-    return [];
   }
+  return testableFiles;
 }
 
-function checkSnapshotTestForPaths(testableFileObjects) {
+function checkSnapshotTestForPaths(testableFileObjects: TestableFileObject[]) {
   testableFileObjects.forEach((testableFileObject) => {
-    try {
-      checkSnapshot(testableFileObject);
-    } catch (error) {
-      console.error(
-        `Error reading file ${testableFileObject.relative}: ${error.message}`
-      );
-    }
+    checkSnapshot(testableFileObject);
   });
 }
 
-function main(dirPath) {
+export function easySnapshot(dirPath: string) {
   const targetDir = path.join(__dirname, dirPath);
 
   console.log("path received: ", dirPath);
@@ -72,6 +61,7 @@ function main(dirPath) {
   checkSnapshotTestForPaths(testableFileObjects);
 }
 
-exports.easySnapshot = function (dirPath) {
-  main(dirPath);
+type TestableFileObject = {
+  relative: string;
+  absolute: string;
 };
